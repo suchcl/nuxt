81a4cb7bb2f349629ad08ddd8d22889f7bb63aa2
DESKTOP-MHSJV2V\ya596 <ya5969@163.com>
2021-11-19 17:58:02 +0800

    init

---
+++ nuxt1/components/Tutorial.vue
@@ -0,0 +1,13 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="reactive">
+    <div class="msg">Hello你好</div>
+  </div>
+</template>
+
+<style scoped>
+.reactive {
+  color: #f20;
+  font-size: 24px;
+}
+</style>
---
+++ nuxt1/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Tutorial />
+    <div class="msg">{{ msg }}</div>
+    <h3>用户信息</h3>
+    <div>姓名：{{ person.name }}</div>
+    <p>年龄：{{ person.age }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Nuxt.js第一个项目",
+      person: {
+        name: "Nicholas Zakas",
+        age: 18,
+      },
+    };
+  },
+};
+</script>
